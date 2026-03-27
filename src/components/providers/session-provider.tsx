12f2e5e022860b4d946785750c6248fb6ef93324
omarchii <omar.chi@anahuacmayab.edu.mx>
2026-03-27 13:46:04 -0600

    fix: force light mode as permanent default

--- src/components/providers/session-provider.tsx
+++ src/components/providers/session-provider.tsx
@@ -5,7 +5,7 @@ import { ThemeProvider } from "next-themes";
 
 export function NextAuthProvider({ children }: { children: React.ReactNode }) {
     return (
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <ThemeProvider attribute="class" defaultTheme="light" enableSystem={false}>
             <SessionProvider>{children}</SessionProvider>
         </ThemeProvider>
     );
